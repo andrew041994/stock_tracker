c348ec69861ac97e46d22d63eae1faf49afc234e
andrew041994 <andrew041994@gmail.com>
2020-06-22 01:07:27 -0400

    refactor code

--- project_frontend/src/containers/HomeContainer.js
+++ project_frontend/src/containers/HomeContainer.js
@@ -11,18 +11,18 @@ class HomeContainer extends Component {
 
     render() {
         return(
-            <div  style={{"padding": "50px"}} >
+            <div  style={{"padding": "30px"}} >
                 <Container fluid>
                 
                 <Row>
-                    <Col md={{ span: 3, offset: 4 }} > <h3 style={{"margin": "auto", "width": "100%", "padding": "20px",
-      "color": "green"}}><strong>Welcome To Stock Tracker</strong></h3></Col>                
+                    <Col lg={{ span: 3, offset: 4 }} > <h3 style={{"margin": "auto", "width": "130%", "padding": "40px",
+                      "color": "green"}}><strong>Welcome To Stock Tracker</strong></h3></Col>                
                 </Row>
                 <Row>
                     <Col md={{ span: 3, offset: 4 }} > < ViewAll /></Col>
                 </Row>
                 <Row >
-                    <Col md={{ offset: 2 }}> 
+                    <Col md={{ offset: 1 }}> 
                      <TrendingStock />                    
                     </Col> 
 
