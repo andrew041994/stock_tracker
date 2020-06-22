8ba617da4bedcfa31cd9868a500135bb27d7bdeb
andrew041994 <andrew041994@gmail.com>
2020-06-22 10:15:24 -0400

    refactor code

--- project_frontend/src/components/ViewButton.js
+++ project_frontend/src/components/ViewButton.js
@@ -1,37 +1,24 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 
 
 class ViewButton extends Component {
 
-    // handleClick = (e) => {
-    // // e.preventDefault()
-    //     // console.log(this.props.stock)
-    //     this.props.getStock(this.props.stock.id)
-
-
-    // }
+   
 
 
   render() {
     return (
       <div> 
         <Link to={'/stocks/' + this.props.stock.id}>
-        <Button variant="info">
-                    View Stock
-                     </Button>   
+        <Button variant="info">View Stock</Button>   
         </Link>
       </div>
     );
   }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         getStock: (id) => dispatch(getStock(id))
-//     }
-// }
+
 
 export default ViewButton
